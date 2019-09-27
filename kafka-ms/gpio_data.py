ade55ae1a0beb664cfbea1469ed379b39a511c77
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2019-09-27 18:40:45 +0530

    Kafka subscriber and publisher

---
+++ kafka-ms/gpio_data.py
@@ -0,0 +1,16 @@
+from gpiozero import (
+        CPUTemperature, PingServer,
+        LoadAverage, DiskUsage
+    )
+
+cpu = CPUTemperature()
+print(cpu.temperature)
+
+# ps = PingServer(host="localhost")
+# print(ps.pin_factory)
+
+la = LoadAverage()
+print(la.load_average)
+
+du = DiskUsage()
+print(du.value)
---
+++ kafka-ms/producer.py
@@ -0,0 +1,22 @@
+import time
+import json
+import datetime
+from gpiozero import CPUTemperature, LoadAverage
+from kafka import KafkaProducer
+
+producer = KafkaProducer(
+    bootstrap_servers="localhost:9092",
+    value_serializer=lambda x: json.dumps(x).encode('utf-8'),
+    request_timeout_ms=10000
+    )
+
+while True:
+    cpu = CPUTemperature()
+    la = LoadAverage()
+    producer.send("temp-hum", {
+        "temp": cpu.temperature,
+        "load": la.load_average,
+        "time": str(datetime.datetime.now())
+        })
+    time.sleep(1)
+    
