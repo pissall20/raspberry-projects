f2022b8f8fcb0e04992fe57ce17ef466f612bdef
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2019-09-30 15:04:28 +0530

    Sockets (#2)
    * Kafka subscriber and publisher
    
    * EOF
    
    * Added socket test script
    
    * Added a listening server and publishing client using socket

---
+++ socket-ex/server.py
@@ -0,0 +1,23 @@
+import socket			 
+import time
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)		 
+print("Socket successfully created")
+
+host = "127.0.0.1"
+port = 12345				
+
+s.bind((host, port))
+s.listen(5)
+# Establish connection with client. 
+conn, addr = s.accept()	 
+print 'Got connection from', addr 
+# send a thank you message to the client. 
+conn.send('Thank you for connecting') 
+
+while True:
+    print(conn.recv(1024).decode("ascii"))
+    # Close the connection with the client 
+    # c.close() 
+    time.sleep(1)
+
