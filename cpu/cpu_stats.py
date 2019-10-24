a60ba47ba9d225e0c3cf12638bbd5c4e0127c8e6
Siddhesh Pisal <siddhesh.pisal@fractal.ai>
2019-10-24 20:19:38 +0530

    Stats from CPU using psutil

---
+++ cpu/cpu_stats.py
@@ -0,0 +1,88 @@
+import psutil
+import datetime
+
+from cpu.utils import get_ip
+
+IP_ADDRESS = get_ip()
+
+"""
+CPU
+"""
+n_cpu = psutil.cpu_count()
+
+cpu_ids = [f"cpu_{id}" for id in range(1, n_cpu + 1)]
+
+total_cpu_usage_time = psutil.cpu_times()
+all_cpu_usage_time = list(
+    map(
+        lambda x, y: {"id": y, "user": x.user, "system": x.system, "idle": x.idle},
+        psutil.cpu_times(percpu=True),
+        cpu_ids,
+    )
+)
+
+total_cpu_usage_percent = psutil.cpu_percent()
+all_cpu_usage_percent_dict = {
+    k: v for k, v in zip(cpu_ids, psutil.cpu_percent(percpu=True))
+}
+
+load_avg_dict = {
+    k: v
+    for k, v in zip(
+        ["1_min", "5_min", "15_min"],
+        [x / psutil.cpu_count() * 100 for x in psutil.getloadavg()],
+    )
+}
+
+"""
+MEMORY
+"""
+virtual_memory_stats = {
+    k: (v / (1024 * 1024)) if k != "percent" else v
+    for (k, v) in psutil.virtual_memory()._asdict().items()
+}
+
+swap_memory_stats = {
+    k: (v / (1024 * 1024)) if k != "percent" else v
+    for (k, v) in psutil.swap_memory()._asdict().items()
+    if k != "percent"
+}
+
+
+"""
+NETWORK
+"""
+net_io = dict(psutil.net_io_counters()._asdict())
+
+
+"""
+STORAGE
+"""
+disk_usage = {
+    k: (v / (1024 * 1024)) if k != "percent" else v
+    for k, v in psutil.disk_usage("/")._asdict().items()
+    if k != "percent"
+}
+
+"""
+SENSOR
+"""
+try:
+    temp = psutil.sensors_temperatures()
+    fans = psutil.sensors_fans()
+except AttributeError:
+    pass
+
+try:
+    battery = psutil.sensors_battery()
+except AttributeError:
+    pass
+
+"""
+OTHER
+"""
+logged_users = list(map(lambda x: x.name, psutil.users()))
+
+boot_time = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")
+
+n_processes = len(psutil.pids())
