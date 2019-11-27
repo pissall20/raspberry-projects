800dab790d04d90181bb13f03e8b57ba5ff604e3
Siddhesh Pisal <siddhesh.pisal@fractal.ai>
2019-11-27 13:58:37 +0530

    Updated subscriber and docker-compose

--- mqtt/sub_dk/subscriber.py
+++ mqtt/sub_dk/subscriber.py
@@ -3,7 +3,8 @@ import time
 import sys
 from datetime import datetime
 import json
-from kafka import KafkaProducer
+#from kafka import KafkaProducer
+import psycopg2
 
 broker = "mqtt-broker"
 topic = "mqtt-test"
@@ -12,17 +13,17 @@ bootstap_server_ip = "kafka"
 
 kafka_topic = "xyz-temp"
 
-producer = KafkaProducer(
-        bootstap_servers=f"{bootstrap_server_ip}:9092",
-        value_serializer=lambda x: json.dumps(x).encode("utf-8"),
-        request_timeout_ms=10000
-    )
+#producer = KafkaProducer(
+#        bootstap_servers=f"{bootstrap_server_ip}:9092",
+#        value_serializer=lambda x: json.dumps(x).encode("utf-8"),
+#        request_timeout_ms=10000
+#    )
 
 def on_message(client, userdata, message):
     print("Recieved data is:")
     print(json.loads(message.payload))
     recv_data = json.loads(message.payload)
-    producer.send(kafka_topic, recv_data)
+    # producer.send(kafka_topic, recv_data)
 
 client = paho.Client("user")
 client.on_message = on_message
