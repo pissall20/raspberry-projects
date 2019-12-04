e8dcdec93b7521e96f4ff963ecce56fdbdd844fa
Siddhesh Pisal <siddhesh.pisal@fractal.ai>
2019-12-04 22:07:38 +0530

    Wrote code to add data to database

--- mqtt/sub_dk/subscriber.py
+++ mqtt/sub_dk/subscriber.py
@@ -1,29 +1,39 @@
-import paho.mqtt.client as paho
-import time
-import sys
-from datetime import datetime
 import json
-#from kafka import KafkaProducer
-import psycopg2
+from datetime import datetime
+
+import paho.mqtt.client as paho
+
+from .create_db_setup import ConnectDatabase
 
 broker = "172.16.18.71"
 topic = "mqtt-test"
 
-bootstap_server_ip = "kafka"
+db_params = {
+    "db_name": "iot_mqtt_test",
+    "host": "timescaledb",
+    "user": "postgres",
+    "password": "postgres",
+    "db_table_name": "temp_humidity"
+}
 
-kafka_topic = "xyz-temp"
+time_pk = "datetime"
+table_name = "temp_hum"
 
-#producer = KafkaProducer(
-#        bootstap_servers=f"{bootstrap_server_ip}:9092",
-#        value_serializer=lambda x: json.dumps(x).encode("utf-8"),
-#        request_timeout_ms=10000
-#    )
+db_conn = ConnectDatabase(**db_params)
+db_conn.create_table(time_pk, table_name)
+
 
 def on_message(client, userdata, message):
     print("Recieved data is:")
     print(json.loads(message.payload))
     recv_data = json.loads(message.payload)
-    # producer.send(kafka_topic, recv_data)
+    recv_data['datetime'] = datetime.now()
+    columns = ",".join(recv_data.keys())
+    values = "VALUES({})".format(",".join(recv_data.values()))
+    insert_stmt = "INSERT INTO {} ({}) {}".format(table_name, columns, values)
+    cursor = db_conn.connection.cursor()
+    cursor.execute(insert_stmt)
+
 
 client = paho.Client("user")
 client.on_message = on_message
