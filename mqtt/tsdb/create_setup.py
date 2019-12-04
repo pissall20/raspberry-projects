20b2130b89b84a1f5c4930b698561bb42da4d251
Siddhesh Pisal <siddhesh.pisal@fractal.ai>
2019-12-04 12:13:34 +0530

    Combined two files

--- mqtt/tsdb/create_setup.py
+++ mqtt/tsdb/create_setup.py
@@ -1,6 +1,23 @@
 import psycopg2 as pg
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 db_name = "iot_mqtt_test"
+
+con = pg.connect(user="postgres", password="postgres", host="localhost")
+con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+create_db_cmd = f"CREATE DATABASE {db_name};"
+cur = con.cursor()
+
+try:
+    cur.execute(create_db_cmd)
+except (Exception, pg.Error) as e:
+    print(e)
+cur.close()
+con.close()
+
+"""DATABASE CREATION IS DONE"""
+
 table_name = "temp_hum"
 
 connection = pg.connect(user="postgres", password="postgres", database=db_name, host="localhost")
@@ -23,3 +40,5 @@ tables = cursor.fetchall()
 
 for table in tables:
     print(table)
+
+"""TABLE CREATION DONE"""
