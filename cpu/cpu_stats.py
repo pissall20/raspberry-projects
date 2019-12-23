e30cf0456b8bbc04bbb869be3ae86d1d08135a4d
Siddhesh Pisal <siddhesh.pisal@fractal.ai>
2019-12-23 14:09:12 +0530

    Added function to get process info sorted by memory used

--- cpu/cpu_stats.py
+++ cpu/cpu_stats.py
@@ -106,3 +106,29 @@ boot_time = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime(
 )
 
 n_processes = len(psutil.pids())
+
+"""
+Get list of processes sorted by memory used
+"""
+
+
+def process_sorted_by_memory():
+    """
+    Get list of running processes sorted by Memory Usage
+    """
+    list_of_processes = []
+    # Iterate over the list
+    for proc in psutil.process_iter():
+        try:
+            # Fetch process details as dict
+            pinfo = proc.as_dict(attrs=['pid', 'name', 'username'])
+            pinfo['memory_used'] = proc.memory_info().vms / (1024 * 1024)
+            # Append dict to list
+            list_of_processes.append(pinfo)
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+    # Sort list of dict by key vms i.e. memory usage
+    list_of_process_objs = sorted(list_of_processes, key=lambda process: process['memory_used'], reverse=True)
+
+    return list_of_process_objs
